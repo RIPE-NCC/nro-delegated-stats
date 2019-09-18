88215b8269465ba7751432dd0f4faaea150f3872
Adianto Wibisono <awibisono@ripe.net>
2019-09-18 17:13:16 +0200

    Comma in test result

--- src/test/scala/net/ripe/rpki/nro/RangeMapTest.scala
+++ src/test/scala/net/ripe/rpki/nro/RangeMapTest.scala
@@ -18,29 +18,29 @@ class RangeMapTest extends FlatSpec{
       map.put(Range.closed(11,20), "B")
       map.put(Range.closed(21,30), "C")
 
-      assert(map.asMapOfRanges().asScala.mkString(" ") == "[1..10] -> A [11..20] -> B [21..30] -> C")
+      assert(map.asMapOfRanges().asScala.mkString(", ") == "[1..10] -> A, [11..20] -> B, [21..30] -> C")
   }
 
   it should "replace and exact overlaps" in {
     map.put(Range.closed(21,30), "D")
-    assert(map.asMapOfRanges().asScala.mkString(" ") == "[1..10] -> A [11..20] -> B [21..30] -> D")
+    assert(map.asMapOfRanges().asScala.mkString(", ") == "[1..10] -> A, [11..20] -> B, [21..30] -> D")
   }
 
   it should "split in case there is a partial overlap" in {
     map.put(Range.closed(21,25), "E")
-    assert(map.asMapOfRanges().asScala.mkString(" ") == "[1..10] -> A [11..20] -> B [21..25] -> E (25..30] -> D")
+    assert(map.asMapOfRanges().asScala.mkString(", ") == "[1..10] -> A, [11..20] -> B, [21..25] -> E, (25..30] -> D")
   }
 
   it should "replace ranges overlapped by new bigger range" in {
     map.put(Range.closed(1,20), "AB")
-    assert(map.asMapOfRanges().asScala.mkString(" ") == "[1..20] -> AB [21..25] -> E (25..30] -> D")
+    assert(map.asMapOfRanges().asScala.mkString(", ") == "[1..20] -> AB, [21..25] -> E, (25..30] -> D")
   }
 
   it should "sometimes produces empty interval when splitting" in {
     map.put(Range.closed( 1,10), "WEIRD1")
     map.put(Range.closed(11,20), "WEIRD2")
-    assert(map.asMapOfRanges().asScala.mkString(" ") ==
-      "[1..10] -> WEIRD1 (10..11) -> AB [11..20] -> WEIRD2 [21..25] -> E (25..30] -> D")
+    assert(map.asMapOfRanges().asScala.mkString(", ") ==
+      "[1..10] -> WEIRD1, (10..11) -> AB, [11..20] -> WEIRD2, [21..25] -> E, (25..30] -> D")
     // (10..11) is actually an empty interval.
   }
 
