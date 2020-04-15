ee7b00f5cf49403f23f473fea44c6d3a58344c76
Adianto Wibisono <awibisono@ripe.net>
2020-04-15 09:51:25 +0200

    For now this is the easiest way to fetch old data, and then run normally. Code will not refetch

---
+++ fetch.sh
@@ -0,0 +1,25 @@
+if [[ $# -lt 3 ]]; then 
+    echo "Usage, fetch YYYY MM DD"
+fi
+
+YYYY=$1
+MM=$2
+DD=$3
+BB=$((DD-1))
+
+THE_DAY=$YYYY$MM$DD
+DAY_BEFORE=$YYYY$MM$BB
+
+mkdir -p data/$THE_DAY
+
+cd data/$THE_DAY
+
+curl http://ftp.apnic.net/stats/apnic/$YYYY/delegated-apnic-extended-$THE_DAY.gz --output apnic.gz
+curl http://ftp.arin.net/pub/stats/arin/delegated-arin-extended-$DAY_BEFORE --output arin
+curl http://ftp.afrinic.net/stats/afrinic/$YYYY/delegated-afrinic-extended-$THE_DAY --output afrinic
+curl http://ftp.lacnic.net/pub/stats/lacnic/delegated-lacnic-extended-$DAY_BEFORE --output lacnic
+curl https://ftp.ripe.net/pub/stats/ripencc/$YYYY/delegated-ripencc-extended-$DAY_BEFORE.bz2 --output ripencc.bz2 
+
+bunzip2 ripencc.bz2
+gunzip apnic.gz
+
