fc70feb9b07b9e165931ce5003576583faa5408d
Adianto Wibisono <awibisono@ripe.net>
2021-07-22 11:46:22 +0200

    Not rsyncing to nro target anymore (dragonstone decommisioned)

--- scripts/generate-script.sh
+++ scripts/generate-script.sh
@@ -1,11 +1,11 @@
+#!/usr/bin/env bash
+
 cat << EOF
 BASE_DIR=$CI_BASE_DIR
-NRO_TARGET=$CI_NRO_TARGET
 RESULT_FTP_PATH=$CI_RESULT_FTP_PATH
 
 AWS_PROFILE=$CI_AWS_PROFILE
 AWS_TARGET=$CI_AWS_TARGET
-export RSYNC_PASSWORD=$CI_RSYNC_PASSWORD
 EOF
 
 
@@ -26,8 +26,6 @@ fi
 java -Denvironment=production -Dconfig.file=./application.conf -jar nro-delegated-stats.jar 1>> log/nro-stats-$TODAY.log  2>> log/nro-stats.err
 
 rsync -rtv $BASE_DIR/result/ $RESULT_FTP_PATH 1>> log/nro-stats-$TODAY.log  2>> log/nro-stats.err
-
-rsync $NRO_SOURCE $NRO_TARGET 1>> log/nro-stats-$TODAY.log  2>> log/nro-stats.err
 
 /usr/local/bin/aws --profile $AWS_PROFILE s3 cp $NRO_SOURCE $AWS_TARGET   1>> log/nro-stats.log 2>> log/nro-stats.err
 EOF
